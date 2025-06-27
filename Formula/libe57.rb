7770a602e8de85d9950679149bc9e8455b1b7232
Martin Aumüller <aumuell@reserv.at>
2025-06-27 22:35:44 +0200

    libe57: update for cmake 4

--- Formula/libe57.rb
+++ Formula/libe57.rb
@@ -1,8 +1,8 @@
 class Libe57 < Formula
   desc "Software Tools for Managing E57 Point Cloud Files"
   homepage "http://libe57.org"
-  url "https://github.com/hlrs-vis/libe57.git", branch: "main", revision: "6c6b0c8355d870342f1736bfcc3b5299fe012d4c"
-  version "1.1.334"
+  url "https://github.com/hlrs-vis/libe57.git", branch: "main", revision: "480038a0785f4dd4bb3494359ae3f69c1944fc60"
+  version "1.1.336"
   license "http://libe57.org/license.html"
   head "https://github.com/hlrs-vis/libe57.git", branch: "main"
 
