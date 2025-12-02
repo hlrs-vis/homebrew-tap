31da8c2cea35620402aba3c1e068db17b311e275
Martin Aumüller <aumuell@reserv.at>
2025-12-02 14:38:39 +0100

    fix compilation with apple clang 17

--- Formula/openinventor.rb
+++ Formula/openinventor.rb
@@ -1,10 +1,11 @@
 class Openinventor < Formula
   homepage 'http://oss.sgi.com/projects/inventor/'
-  url 'https://github.com/aumuell/open-inventor.git', :using => :git, :revision => '382052ed890109aac1993a2627b8df938e31ce04'
+  url 'https://github.com/aumuell/open-inventor.git', :using => :git, :revision => '37521a12090ae48fbd2863e26da0f5babf6a2dbd'
   head 'https://github.com/aumuell/open-inventor.git', :using => :git
   version '2.1.6'
 
   depends_on "cmake" => :build
+  depends_on "bison" => :build
 
   depends_on "openmotif"
   depends_on "libxi"
@@ -12,6 +13,7 @@ class Openinventor < Formula
   depends_on "mesa-glu"
   depends_on "mesalib-glw"
   depends_on "jpeg"
+  depends_on "freetype"
 
   def install
     mkdir "brew-build" do
