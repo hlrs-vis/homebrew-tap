51baaae674eab831d348f55afb1cdafc7dfd6f0a
Martin Aumüller <aumuell@reserv.at>
2026-01-20 11:37:55 +0100

    libe57: update to 1.1.337

--- Formula/libe57.rb
+++ Formula/libe57.rb
@@ -1,8 +1,8 @@
 class Libe57 < Formula
   desc "Software Tools for Managing E57 Point Cloud Files"
   homepage "http://libe57.org"
-  url "https://github.com/hlrs-vis/libe57.git", branch: "main", revision: "480038a0785f4dd4bb3494359ae3f69c1944fc60"
-  version "1.1.336"
+  url "https://github.com/hlrs-vis/libe57.git", branch: "main", revision: "aa6ecb82d0a50153425649889d6b2fb9e4f4fe79"
+  version "1.1.337"
   license "http://libe57.org/license.html"
   head "https://github.com/hlrs-vis/libe57.git", branch: "main"
 
