2d7a80c3f94e48cb537b70a5467b743f1d0bb47e
Martin Aumüller <aumuell@reserv.at>
2026-04-10 15:46:09 +0200

    covise: more dependencies for audio support

--- Formula/covise.rb
+++ Formula/covise.rb
@@ -46,6 +46,8 @@ class Covise < Formula
   depends_on "hidapi"
   depends_on "eigen"
   depends_on "bullet"
+  depends_on "glm"
+  depends_on "freealut"
   depends_on "hlrs-vis/tap/osgephemeris"
   depends_on "hlrs-vis/tap/osgcal"
   depends_on "hlrs-vis/tap/opencrg"
